4f68d47ef8311a644f7b3eb66307d6183590cd41
Alex Kozik <alex.kozik@yahoo.com>
2026-04-17 17:00:06 -0400

    docs: update python binding docs and root smoke script

--- test.py
+++ test.py
@@ -1,4 +1,31 @@
-from ctorch import Matrix, KNN
+from ctorch import (
+    DataAugmentationType,
+    GaussianNB,
+    KMeans,
+    KNN,
+    MAB,
+    Matrix,
+    SVM,
+    UCB,
+)
 
 print(Matrix([[1, 2], [3, 4]]))
 print(KNN(1, [[0, 0], [1, 1]], [[0, 1]]).predict([[0.1, 0.1]]))
+
+x = [[0, 0], [1, 1], [1.2, 0.8], [-0.4, -0.3]]
+y01 = [[0, 1, 1, 0]]
+ypm = [[-1, 1, 1, -1]]
+
+print(GaussianNB(x, y01).predict([[0.9, 0.9]]))
+print(SVM(x, ypm, learning_rate=0.0, max_iter=100, c_value=1.0, augmentation=DataAugmentationType.NO_OP).predict([[0.9, 0.9]]))
+print(KMeans(2, x).assignments)
+
+mab = MAB(3, 0.2)
+arm = mab.select_arm()
+mab.update(arm, 1.0)
+print("mab arm:", arm)
+
+ucb = UCB(3)
+arm2 = ucb.select_arm()
+ucb.update(arm2, 0.5)
+print("ucb arm:", arm2)
