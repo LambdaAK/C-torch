59590927dfe91775ac752eeca21501944829465f
Alex Kozik <alex.kozik@yahoo.com>
2026-04-23 21:10:17 -0300

    Forgot to commit these files earlier

--- test.py
+++ test.py
@@ -1,31 +1,10 @@
-from ctorch import (
-    DataAugmentationType,
-    GaussianNB,
-    KMeans,
-    KNN,
-    MAB,
-    Matrix,
-    SVM,
-    UCB,
-)
+from ctorch import Matrix
 
-print(Matrix([[1, 2], [3, 4]]))
-print(KNN(1, [[0, 0], [1, 1]], [[0, 1]]).predict([[0.1, 0.1]]))
+m = Matrix(100, 100)
+n = Matrix(100, 100)
 
-x = [[0, 0], [1, 1], [1.2, 0.8], [-0.4, -0.3]]
-y01 = [[0, 1, 1, 0]]
-ypm = [[-1, 1, 1, -1]]
+# multiply them
+p = m @ n
 
-print(GaussianNB(x, y01).predict([[0.9, 0.9]]))
-print(SVM(x, ypm, learning_rate=0.0, max_iter=100, c_value=1.0, augmentation=DataAugmentationType.NO_OP).predict([[0.9, 0.9]]))
-print(KMeans(2, x).assignments)
-
-mab = MAB(3, 0.2)
-arm = mab.select_arm()
-mab.update(arm, 1.0)
-print("mab arm:", arm)
-
-ucb = UCB(3)
-arm2 = ucb.select_arm()
-ucb.update(arm2, 0.5)
-print("ucb arm:", arm2)
+# print the result
+print(p)
